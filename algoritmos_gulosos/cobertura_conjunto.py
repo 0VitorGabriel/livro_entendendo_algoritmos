a3b45cddabdbf4b7897e21b1371b971236503d76
0VitorGabriel <victoropo64@outlook.com>
2023-12-01 10:01:53 -0300

    primeiro algoritmo guloso

---
+++ algoritmos_gulosos/cobertura_conjunto.py
@@ -0,0 +1,25 @@
+estados_abranger = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+estacoes = {
+    "kum": set(["id", "nv", "ut"]),
+    "kdois": set(["wa", "id", "mt"]),
+    "ktres": set(["or", "nv", "ca"]),
+    "kquatro": set(["nv", "ut"]),
+    "kcinco": set(["ca", "az"])
+}
+
+estacoes_finais = set()
+
+while estados_abranger:
+    melhor_estacao = None
+    estados_cobertos = set()
+    for estacao, estados_por_estacao in estacoes.items():
+        cobertos = estados_abranger & estados_por_estacao
+        if len(cobertos) > len(estados_cobertos):
+            melhor_estacao = estacao
+            estados_cobertos = cobertos
+
+    estacoes_finais.add(melhor_estacao)
+    estados_abranger -= estados_cobertos
+
+print(estacoes_finais)
